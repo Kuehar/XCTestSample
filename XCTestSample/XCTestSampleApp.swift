2457f18a2fc535d9995389f24f8c0211ac07501f
kuehar <green95251@gmail.com>
2023-10-20 01:09:08 +0900

    Initial Commit

---
+++ XCTestSample/XCTestSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestSampleApp.swift
+//  XCTestSample
+//
+//  Created by kuehar on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct XCTestSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
