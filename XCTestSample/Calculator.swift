589345f36846eb5ba2a0d7a019f27a986d7ae077
kuehar <green95251@gmail.com>
2023-10-20 10:33:31 +0900

    first commit

---
+++ XCTestSample/Calculator.swift
@@ -0,0 +1,8 @@
+//
+//  Calculator.swift
+//  XCTestSample
+//
+//  Created by kuehar on 2023/10/20.
+//
+
+import Foundation
---
+++ XCTestSampleTests/CalculatorTests.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorTests.swift
+//  XCTestSampleTests
+//
+//  Created by kuehar on 2023/10/20.
+//
+
+import Foundation
